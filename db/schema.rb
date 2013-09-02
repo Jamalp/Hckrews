7079fdd5cd31bb2982be86b6563f0ccde1000df5
Wesley Carr <surfswell17@gmail.com>
2013-09-02 15:48:14 -0400

    Removed dummy data

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130831195038) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "posts", force: true do |t|
-    t.string   "title"
-    t.string   "url"
-    t.integer  "points"
-    t.integer  "comments"
-    t.string   "top_comment"
-    t.boolean  "hot"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
