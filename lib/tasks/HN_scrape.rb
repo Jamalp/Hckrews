86a8b4870c0fe20d2af7bf790814c33f238606a0
Jamal Powell <jamalkpowell@gmail.com>
2013-09-03 12:04:27 -0400

    Created rake task

--- lib/tasks/HN_scrape.rb
+++ lib/tasks/HN_scrape.rb
@@ -1,40 +1,25 @@
 require 'rubygems'
 require 'json'
 require 'ruby-hackernews'
-
 include RubyHackernews
 
-posts = Entry.all
-x
-posts.map! do |post|
-  { :title => post.link.title,
-    :site => post.link.site,
-    :href => post.link.href,
-    :number => post.number,
-    :score => post.voting.score,
-    :user => post.user.name,
-    :time => post.time
-    :comments => post.comments_count,
-    :post_id => post.id,
-end
-
-time = Time.now.gmtime
-filename = File.dirname(__FILE__) + "/public/data/"
-now_filename = filename + "now.json"
-filename += "#{time.year}-#{sprintf("%.3d", time.month)}-#{sprintf("%.3d", time.mday)}"
-daily_filename = filename + ".json"
-hourly_filename = filename + "-#{sprintf("%.2d", Time.now.hour)}.json"
-
-
-puts "Writing #{now_filename}"
-File.open(now_filename, "w") { |f| f.puts posts.to_json }
-
-unless File.exist?(daily_filename)
-  puts "Writing #{daily_filename}"
-  File.open(daily_filename, "w") { |f| f.puts posts.to_json }
-end
-
-unless File.exist?(hourly_filename)
-  puts "Writing #{hourly_filename}"
-  File.open(hourly_filename, "w") { |f| f.puts posts.to_json }
+namespace :hn_rake do
+  desc "Using HN Gem to Pull posts from the front page"
+  task posts: :environment do
+    # Query hackernews with entry.all
+    # store each returned entry as a post
+    Entry.all(8).each do |post|
+      post_data = { title: post.link.title,
+                    description: post.link.text,
+                    site: post.link.site,
+                    href: post.link.href,
+                    score: post.voting.score,
+                    user: post.user.name,
+                    comments_count: post.comments_count,
+                    hn_post_id: post.id,
+                    created_at: post.time
+                  }
+      post = Post.create(post_data)
+    end
+  end
 end
